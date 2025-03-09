0f3e5493540e2aefee6b4ff788f89784aa1afdac
allan.debiaggio <allan.de-biaggio@laplateforme.io>
2025-03-09 12:55:09 +0100

    Working random list of naturals

--- main.py
+++ main.py
@@ -6,3 +6,25 @@ Implement a program that :
 same, don't follow themselves)
 - Maybe one natural and one accidental for the list, with changing order
 """
+import random
+
+def main() :
+    print("Hello and welcome to the Music Companion !")
+    # user = input("Choose an option")
+    print(random_list())
+
+def random_list() :
+    naturals = ["A","B","C","D","E","F","G"]
+    sharps = ["A#","C#","D#","F#","G#"]
+    flats = ["Ab","Bb","Db","Eb","Gb"]
+    random_notes = []
+    while len(random_notes) < 7 :
+        index = random.randint(0,len(naturals)-1)
+        if naturals[index] in random_notes :
+            continue
+        else :
+            random_notes += (naturals[index])
+    return random_notes
+
+
+main()
